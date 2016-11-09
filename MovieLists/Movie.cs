9a9c4c53ae72fa4c23d653bfbf1261ea82e5c958
tundelola <foladayo@aol.com>
2016-11-08 21:53:14 -0500

    first commit

---
+++ MovieLists/Movie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLists
+{
+    class Movie
+    {
+        private string Title;
+        private string Category;
+        //properties of the class field
+        public string TITLE
+        {
+            get
+            {
+                return Title;
+            }
+
+            set
+            {
+                Title = value;
+            }
+        }
+        //properties of the class field
+        public string CATEGORY
+        {
+            get
+            {
+                return Category;
+            }
+
+            set
+            {
+                Category = value;
+            }
+        }
+        //Makes a class constructor for the field
+        public Movie(string title, string category)
+        {
+            this.Title = title;
+            this.Category = category;
+        }
+    }
+}
---
+++ MovieLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList MovieL = new ArrayList();
+            MovieL.Add(new Movie("Citizen Kane", "drama"));
+            MovieL.Add(new Movie("Casablanca", "drama"));
+            MovieL.Add(new Movie("The Godfather", "drama"));
+            MovieL.Add(new Movie("Gone With The Wind", "drama"));
+
+            MovieL.Add(new Movie("Snow White And The Seven Dwarfs", "animated"));
+            MovieL.Add(new Movie("Fantasia", "animated"));
+
+            MovieL.Add(new Movie("King Kong", "horror"));
+            MovieL.Add(new Movie("Jaws", "horror"));
+
+            MovieL.Add(new Movie("A Clockwork Orange", "scifi"));
+            MovieL.Add(new Movie("Close Encounters Of The Third Kind", "scifi"));
+
+
+        }
+        public void Search(ArrayList MovieLis, string category)
+        {
+
+            for (int i = 0; i < MovieLis.Count; i++)
+            {
+                if (MovieLis[i].category == category)
+                {
+                    Console.WriteLine("I found the Movie {0} ", MovieLis[i].title);
+
+                }
+            }
+        }
+    }
+}
